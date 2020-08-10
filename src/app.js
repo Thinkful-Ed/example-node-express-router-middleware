075708c481138faef12892e1d66ada0723b0a877
Wes Reid <bwreid@gmail.com>
2020-08-10 11:46:27 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+// Router-level middleware
+const checkForAbbreviationLength = (req, res, next) => {
+  const abbreviation = req.params.abbreviation;
+  if (abbreviation.length !== 2) {
+    next(`State abbreviation "${abbreviation}" is invalid.`);
+  } else {
+    next();
+  }
+};
+
+// Routes
+app.use(
+  "/states/:abbreviation",
+  checkForAbbreviationLength,
+  (req, res, next) => {
+    res.send(`${req.params.abbreviation} is a nice state, I'd like to visit.`);
+  }
+);
+
+app.use(
+  "/travel/:abbreviation",
+  checkForAbbreviationLength,
+  (req, res, next) => {
+    res.send(`Enjoy your trip to ${req.params.abbreviation}!`);
+  }
+);
+
+// Error handlers
+app.use((req, res, next) => {
+  res.send(`The route ${req.path} does not exist!`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.send(err);
+});
+
+module.exports = app;
